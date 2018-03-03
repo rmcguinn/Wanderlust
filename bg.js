690377058fdd353aff839193e222f0fad61f9e50
Ryan <rmcguinn94@gmail.com>
2018-03-03 14:07:12 -0800

    Made site responsive
    - Made the site responsive from 320px wide to basically anything (could use a bit of work for giant resolutions however.)
    
    - Added media queries for phones, small tablets, medium tablets and from 962px - 1300px wide, which was breaking the design in that range.
    
    - Optimized some of the images even further to hit under 1mb (still a WiP)
    
    - Removed some of the images from the bgimages array that didn't fit the design as well.
    
    - Fixed a bug keeping normalize.css from loading.

--- bg.js
+++ bg.js
@@ -1,16 +1,17 @@
-const bgimages = ['cave-2', 'city-fence-2', 'cliff', 'fishing', 'overlooking-city', 'red-mountains-no', 'stars', 'trees', 'water-rocks', 'window'];
+// const bgimages = ['cave-2', 'city-fence-2', 'cliff', 'fishing', 'overlooking-city', 'red-mountains-no', 'stars', 'trees', 'water-rocks', 'window'];
+const bgimages = ['city-fence-2', 'cliff', 'overlooking-city', 'red-mountains-no', 'trees', 'water-rocks'];
 
 
 window.onload = function init() {
     const random = Math.floor(Math.random() * bgimages.length);
     console.log(random);
     bg1 = bgimages[random];
-    if (random == 5) {
-        document.documentElement.style.background = 'url(img/' + bg1 + '.jpg)';
+    if (random == 3) {
+        document.documentElement.style.background = 'url(img/' + bg1 + '.jpg), #1a1a1a';
         document.documentElement.style.backgroundSize = 'cover';
         document.documentElement.style.backgroundPosition = 'center';
     } else {
-    document.documentElement.style.background = ' linear-gradient( rgba(0, 0, 0, 0.5), rgba(0, 0, 0, 0.5)), url(img/' + bg1 + '.jpg)';
+    document.documentElement.style.background = 'linear-gradient( rgba(0, 0, 0, 0.5), rgba(0, 0, 0, 0.5)), url(img/' + bg1 + '.jpg)';
     document.documentElement.style.backgroundSize = 'cover';
     document.documentElement.style.backgroundPosition = 'center';
     };
